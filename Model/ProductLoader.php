1af47c339b7528e61f8908111c37b4fd25c785d1
Elia Cools <eliacools@hotmail.com>
2021-03-23 15:53:24 +0100

    add productloader

--- Model/ProductLoader.php
+++ Model/ProductLoader.php
@@ -4,4 +4,17 @@
 class ProductLoader
 {
 
+    public static function getProduct(PDO $pdo, int $id):? array
+    {
+        $query = $pdo->prepare('SELECT * FROM calculator.product WHERE product.id = :id');
+        $query->bindValue('id', $id);
+        $query->execute();
+        return $query->fetch();
+    }
+
+    public static function getProducts(PDO $pdo): array
+    {
+        $query = $pdo->query('SELECT * FROM calculator.product');
+        return $query->fetchAll();
+    }
 }
