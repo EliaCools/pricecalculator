1baef8eb5fc518baca95bb2c9b1ed15bb0f8bd48
Alexandra Banica <banica9221@hotmail.be>
2021-03-24 16:01:22 +0100

    small changes

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -19,13 +19,14 @@ class HomepageController
 
 
         $customerLoader = new CustomerLoader();
+        $productLoader = new ProductLoader();
         $singleCustomer = $customerLoader->singleCustomer($this->db, 3);
+        $customers = $customerLoader->allCustomers($this->db);
 
-            $products = ProductLoader::getProducts($this->db);
+            $products = $productLoader->getProducts($this->db);
             $customer = new Customer((int)$singleCustomer['id'], $singleCustomer['firstname'],$singleCustomer['lastname'], (int)$singleCustomer['group_id'],(int)$singleCustomer['fixed_discount'],(int)$singleCustomer['variable_discount']);
 
             $calculator->comparePercentage($this->db,$customer->getId(),$customer->getFirstName(),$customer->getLastName(),$customer->getGroupId(),$customer->getFixDiscount(),$customer->getVarDiscount());
-            var_dump($calculator->comparePercentage($this->db,$customer->getId(),$customer->getFirstName(),$customer->getLastName(),$customer->getGroupId(),$customer->getFixDiscount(),$customer->getVarDiscount()));
             require 'View/homepage.php';
 
 
--- View/homepage.php
+++ View/homepage.php
@@ -1,34 +1,28 @@
-<?php require 'includes/header.php'?>
-<!-- this is the view, try to put only simple if's and loops here.
-Anything complex should be calculated in the model -->
+<?php require 'includes/header.php' ?>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
     </nav>
 
-<section>
+    <section>
+        <form method="post">
+            <select name="productid">
+                <?php foreach ($products as $product):
+                    var_dump($products); ?>
+                    <option value="<?php $product['id']; ?>"><?php echo $product['name'] ?></option>
+                <?php endforeach; ?>
+            </select>
+            <select name="customerid">
+                <?php foreach ($customers as $customer): ?>
+                    <option value="<?php $customer['id']; ?>"><?php echo $customer['firstname'] ?></option>
+                <?php endforeach; ?>
+            </select>
+        </form>
+        <h3></h3>
+    </section>
 
-    <form method="post">
-        <select name="productid" >
-<?php foreach($products as $product):
-    var_dump($products);?>
-            <option value="<?php $product['id'];?>"><?php echo $product['name']?></option>
-<?php endforeach;?>
-        </select>
-
-        <select name="customerid" >
-            <?php foreach($customers as $customer):?>
-                <option value="<?php $customer['id'];?>"><?php echo $customer['firstname']?></option>
-            <?php endforeach;?>
-        </select>
-    </form>
-
-    <h3>
-
-    </h3>
-
-</section>
-<?php require 'includes/footer.php'?>
+<?php require 'includes/footer.php' ?>
