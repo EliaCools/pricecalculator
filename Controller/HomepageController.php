34e9bd5e01c26c3045e80d77689f696444483efb
Elia Cools <eliacools@hotmail.com>
2021-03-24 15:58:26 +0100

    calculator

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -19,14 +19,26 @@ class HomepageController
 
 
         $customerLoader = new CustomerLoader();
-        $singleCustomer = $customerLoader->singleCustomer($this->db, 3);
 
-            $products = ProductLoader::getProducts($this->db);
+        $productloader = new ProductLoader();
+
+
+        $singleProduct = $productloader->getProduct($this->db,3);
+        $product = new Product((int)$singleProduct['id'], $singleProduct['name'], (int)$singleProduct['price']);
+
+
+             $singleCustomer = $customerLoader->singleCustomer($this->db, 10);
             $customer = new Customer((int)$singleCustomer['id'], $singleCustomer['firstname'],$singleCustomer['lastname'], (int)$singleCustomer['group_id'],(int)$singleCustomer['fixed_discount'],(int)$singleCustomer['variable_discount']);
 
-            $calculator->comparePercentage($this->db,$customer->getId(),$customer->getFirstName(),$customer->getLastName(),$customer->getGroupId(),$customer->getFixDiscount(),$customer->getVarDiscount());
-            var_dump($calculator->comparePercentage($this->db,$customer->getId(),$customer->getFirstName(),$customer->getLastName(),$customer->getGroupId(),$customer->getFixDiscount(),$customer->getVarDiscount()));
-            require 'View/homepage.php';
+            //$calculator->comparePercentage($this->db,$customer->getId(),$customer->getFirstName(),$customer->getLastName(),$customer->getGroupId(),$customer->getFixDiscount(),$customer->getVarDiscount());
+            //var_dump($calculator->comparePercentage($this->db,$customer->getId(),$customer->getFirstName(),$customer->getLastName(),$customer->getGroupId(),$customer->getFixDiscount(),$customer->getVarDiscount()));
+            //require 'View/homepage.php';
+
+            //$calculator->percentIsHighestGroup($this->db, $product, $customer);
+            //$calculator->maxVarDiscount($this->db, $customer);
+            $calculator->checkCustomerDiscount($this->db, $product,$customer);
+
+
 
 
     }
--- Model/ProductLoader.php
+++ Model/ProductLoader.php
@@ -3,7 +3,7 @@
 
 class ProductLoader
 {
-    public static function getProduct(PDO $pdo, int $id):? array
+    public function getProduct(PDO $pdo, int $id):? array
     {
         $query = $pdo->prepare('SELECT * FROM product WHERE product.id = :id');
         $query->bindValue('id', $id);
@@ -11,7 +11,7 @@ class ProductLoader
         return $query->fetch();
     }
 
-    public static function getProducts(PDO $pdo): array
+    public  function getProducts(PDO $pdo): array
     {
         $query = $pdo->query('SELECT * FROM product');
         return $query->fetchAll();
